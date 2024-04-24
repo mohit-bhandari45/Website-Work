892db0dda9d37dd99e32c6716f509f49d5fb95d4
mohit-bhandari45 <mohitbhandari852@gmail.com>
2024-04-24 17:47:51 +0530

    added login and signups

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,20 @@
 import React, { useEffect, useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify';
 
 const Login = (props) => {
     const [bool, setbool] = useState(false)
     const [visible, setvisible] = useState(false)
+    const [registerdetails, setregisterdetails] = useState({
+        username: "",
+        password: "",
+        email: ""
+    })
+
+
+    const [logindetails, setlogindetails] = useState({
+        username: "",
+        password: ""
+    })
 
 
     useEffect(() => {
@@ -16,19 +28,66 @@ const Login = (props) => {
     }, [props])
 
 
+    const handleChange = (e) => {
+        // console.log(e.target.value)
+        if (bool) {
+            setlogindetails({ ...logindetails, [e.target.name]: e.target.value })
+        } else {
+            setregisterdetails({ ...registerdetails, [e.target.name]: e.target.value })
+        }
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(e)
-        console.log(e.target)
+        if (registerdetails.email.length > 0) {
+            //register route
+            handleregistervalidation()
+        } else {
+            //login route
+        }
+
+        setregisterdetails({
+            username: "",
+            password: "",
+            email: ""
+        })
+        setlogindetails({
+            username: "",
+            password: ""
+        })
+    }
+
+    const handleregistervalidation = () => {
+
     }
 
 
     const handleButton1 = (e) => {
+        toast('🦄 Wow so easy!', {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            transition: "Bounce",
+        });
         setbool(true)
+        setregisterdetails({
+            username: "",
+            password: "",
+            email: ""
+        })
     }
 
     const handleButton2 = (e) => {
         setbool(false)
+        setlogindetails({
+            username: "",
+            password: ""
+        })
     }
 
 
@@ -48,10 +107,10 @@ const Login = (props) => {
                         </div>
                     </div>
                     <form onSubmit={handleSubmit} className='relative flex flex-col h-[180px]' action="">
-                        <input className={bool ? "py-4 px-4 w-[380px] border rounded-2xl mt-6 text-xl transiton-all duration-150" : "py-3 px-4 w-[380px] border rounded-2xl mb-2 mt-2 text-xl transiton-all duration-150"} placeholder='Username' type="text" name="" id="user" />
-                        <input className={bool ? "py-4 px-4 w-[380px] border rounded-2xl mt-6 text-xl transiton-all duration-150" : "py-3 px-4 w-[380px] border rounded-2xl mb-2 mt-2 text-xl transiton-all duration-150"} placeholder='Password' type="password" name="" id="pass" />
-                        <input className={bool ? "py-3 px-4 hidden w-[380px] border rounded-2xl mb-4 mt-3 text-xl transiton-all duration-150" : "py-3 px-4 block w-[380px] border rounded-2xl mb-2 mt-2 text-xl transiton-all duration-150"} placeholder='Mobile Number' type="number" name="" id="num" />
-                        
+                        <input onChange={handleChange} className={bool ? "py-4 px-4 w-[380px] border rounded-2xl mt-6 text-xl transiton-all duration-150" : "py-3 px-4 w-[380px] border rounded-2xl mb-2 mt-2 text-xl transiton-all duration-150"} placeholder='Username' type="text" name="username" id="user" value={bool ? logindetails.username : registerdetails.username} />
+                        <input onChange={handleChange} className={bool ? "py-4 px-4 w-[380px] border rounded-2xl mt-6 text-xl transiton-all duration-150" : "py-3 px-4 w-[380px] border rounded-2xl mb-2 mt-2 text-xl transiton-all duration-150"} placeholder='Password' type="password" name="password" id="pass" value={bool ? logindetails.password : registerdetails.password} />
+                        <input onChange={handleChange} className={bool ? "py-3 px-4 hidden w-[380px] border rounded-2xl mb-4 mt-3 text-xl transiton-all duration-150" : "py-3 px-4 block w-[380px] border rounded-2xl mb-2 mt-2 text-xl transiton-all duration-150"} placeholder='Email' type="email" name="email" id="num" value={bool ? "" : registerdetails.email} />
+
                         <input className={bool ? "absolute cursor-pointer top-[175px] border-none opacity-0 bg-[#ec8a73] text-white w-[380px] flex justify-center items-center py-4 font-semibold text-2xl border rounded-2xl mb-10 mt-10 transiton-all duration-150" : "absolute cursor-pointer top-[175px] border-none opacity-100 bg-[#ec8a73] text-white w-[380px] flex justify-center items-center py-4 font-semibold text-2xl border rounded-2xl mb-10 mt-10 transiton-all duration-150"} type="submit" value="Signup" />
 
                         <input className={bool ? "absolute cursor-pointer top-[175px] border-none opacity-100 bg-[#ec8a73] text-white w-[380px] flex justify-center items-center py-4 font-semibold text-2xl border rounded-2xl mb-10 mt-10 transiton-all duration-150" : "absolute cursor-pointer top-[175px] border-none opacity-0 bg-[#ec8a73] text-white w-[380px] flex justify-center items-center py-4 font-semibold text-2xl border rounded-2xl mb-10 mt-10 transiton-all duration-150"} type="submit" value="Login" />
@@ -59,9 +118,24 @@ const Login = (props) => {
                 </div>
                 <p className={bool ? "opacity-100 pl-16 text-[blue] transiton-all duration-150" : "opacity-0 pl-16 text-[blue] transiton-all duration-150"}>Forget Password?</p>
                 <div className={bool ? "flex justify-center items-center mt-20 opacity-100 transiton-all duration-150" : "flex justify-center items-center mt-20 opacity-0 transiton-all duration-150"}>
-                    <span>Not a member? <span onClick={()=>setbool(false)} className='text-[blue] cursor-pointer'>Signup now</span></span>
+                    <span>Not a member? <span onClick={() => setbool(false)} className='text-[blue] cursor-pointer'>Signup now</span></span>
                 </div>
             </div>
+            <ToastContainer
+                position="bottom-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
+                transition="Bounce"
+            />
+            {/* Same as */}
+            <ToastContainer />
         </>
     )
 }
